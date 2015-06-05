0b323f682fcff0e2799906251ce46c0c6a0a2462
Chris Piuggi <cpiuggi@method.com>
2015-06-05 15:51:28 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Electron Express.js Hello World
+===============================
+
+First Ensure you have Node.js / npm on your system.
+
+Then make sure you have electron on your system
+
+```npm install electron -g ```
+
+
+
+Getting started:
+
+``` npm install ```
+
+Then make sure to run `./node_modules/.bin/electron-rebuild`
+
+This will provide you with a node_module compatibility for electron, anytime you call npm install you must call `./node_modules/.bin/electron-rebuild`.
---
+++ main.js
@@ -0,0 +1,53 @@
+var app = require('app');  // Module to control application life.
+var BrowserWindow = require('browser-window');  // Module to create native browser window.
+
+var express = require('express');
+var expressApp = express();
+
+expressApp.get('/', function (req, res) {
+  res.send("whats up misa")
+  //res.send('Hello Electron World From Express!');
+});
+
+var server = expressApp.listen(8080, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
+
+// Report crashes to our server.
+require('crash-reporter').start();
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the javascript object is GCed.
+var mainWindow = null;
+console.log('process.platform= '+process.platform);
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+// This method will be called when Electron has done everything
+// initialization and ready for creating browser windows.
+app.on('ready', function() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 1080, height: 1920, /*fullscreen: true, kiosk: true*/});
+
+  // and load the index.html of the app.
+  //mainWindow.loadUrl('file://' + __dirname + '/www/index.html');
+  mainWindow.loadUrl('http://localhost:8080')
+  // Open the devtools.
+  mainWindow.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
